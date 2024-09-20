160985c68c96cd9f7981e1d954e67e04a22b34d9
khalifa-pendrops <dikee5200@gmail.com>
2024-09-20 18:04:13 +0100

    home and header components added

---
+++ src/Components/Header.jsx
@@ -0,0 +1,57 @@
+import React, {useState, useEffect} from "react";
+import { Link } from 'react-router-dom'
+import logo from '../assets/logo.png';
+import './Home.css';
+
+function Header() {
+    const [scrolled, setScrolled] = useState(false);
+
+    const handleScroll = () => {
+        if (window.scrollY > 50) {
+            setScrolled(true);
+        } else {
+            setScrolled(false);
+        }
+    };
+    
+    useEffect(() => { //this is the setup for the event listener on mount component
+        window.addEventListener("scroll", handleScroll);
+            return () => { //this will remove event listener when the component unmounts
+              window.removeEventListener("scroll", handleScroll);
+            };
+    }, []);
+
+
+
+  return (
+    <div>
+      <header className={`head-main-div ${scrolled ? "scrolled" : " "}`}>
+        <a href="/">
+          <img src={logo} alt="logo" width="60px" />
+        </a>
+        <nav className="head-nav">
+          <li className="home-nav-list">
+            <a href="#home">Home</a>
+          </li>
+          <li>
+            {" "}
+            <a href="#about">About</a>
+          </li>
+          <li>
+            {" "}
+            <a href="#services">Services</a>
+          </li>
+          <li>
+            {" "}
+            <a href="#pricing">Pricing</a>
+          </li>
+          <Link to="/contact-us" className="links">
+            <li>Contact Us</li>
+          </Link>
+        </nav>
+      </header>
+    </div>
+  );
+}
+
+export default Header;
